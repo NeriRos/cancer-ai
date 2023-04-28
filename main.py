fb3141f3981c63467ce86e42e0f8adfa84ef4495
Neriya Rosner <neri.coder@gmail.com>
2023-04-29 00:35:31 +0300

    remove raw text import

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from langchain import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
 
-from src.prepare_data import get_state_of_union, get_vector_db, get_content_chunks_documents
+from src.prepare_data import get_vector_db, get_content_chunks_documents
 
 
 def main():
     llm = OpenAI(temperature=0)
     embeddings = OpenAIEmbeddings()
 
-    text = get_state_of_union()
     content_chunks_documents = get_content_chunks_documents()
 
     vector_db = get_vector_db(content_chunks_documents, embeddings)
