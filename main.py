9248eef2331851a57729d4abd152b95be8edebc9
Neriya Rosner <neri.coder@gmail.com>
2023-04-29 00:33:55 +0300

    move login to function

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from sys import argv
-
 from langchain import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
 
-from data_querying import generate_insights
-from prepare_data import get_state_of_union, get_vector_db, get_content_chunks_documents
+from src.prepare_data import get_state_of_union, get_vector_db, get_content_chunks_documents
 
-if __name__ == '__main__':
+
+def main():
     llm = OpenAI(temperature=0)
     embeddings = OpenAIEmbeddings()
 
@@ -15,9 +13,9 @@ if __name__ == '__main__':
 
     vector_db = get_vector_db(content_chunks_documents, embeddings)
 
-    if argv[1] == "insights":
-        question = argv[2]
+    # generate_insights(llm, vector_db, text)
+    return llm, vector_db
 
-        result = generate_insights(llm, vector_db, question)
 
-        print(result)
+if __name__ == '__main__':
+    main()
